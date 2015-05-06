dc3f4024f5d04d6ac84448395141ccbe2482b005
parimaldeep <parimaldeep@gmail.com>
2015-05-05 23:05:35 -0500

    added changes for nesters

--- spectralclustering-1.1/runThissc.m
+++ spectralclustering-1.1/runThissc.m
@@ -1,21 +1,26 @@
 % data_set = 'rcv';
 data_set = 'corel';
+% sel_item = 5000;
 
 input_file = ['data/', num2str(data_set), '_feature.mat'];
 load(input_file);
+
 input_file = ['data/', num2str(data_set), '_label.mat'];
 load(input_file);
 
+% feature = feature(1:sel_item, :);
+% label = label(1:sel_item, :);
 
-list_t = [5 10 15 20 50 100 150 200];
+list_t = [20 50 100 200 500 1000 1500];
 result_mat_sc = zeros(numel(list_t), 3);
 
 sigma = 20;
 num_clusters = 18;
 block_size = 10;
 for i = 1:numel(list_t)
-    tic;
-%     profile -timer cpu;
+    tStart = tic;
+%     profile clear;
+%     profile on -detail builtin -history -memory;
     t = list_t(i);
     gen_nn_distance(feature, t, block_size, 0);
 
@@ -26,9 +31,11 @@ for i = 1:numel(list_t)
     
     accuracy_score = accuracy(label, cluster_labels);
     
-    iteration_time = toc;
-    result_mat(i, :) = [t ,accuracy_score, iteration_time];
+    iteration_time = toc(tStart);
+    result_mat_sc(i, :) = [t ,accuracy_score, iteration_time];
 %     S = profile('status');
-    profile clear;
+%     profile off;
+%     profile viewer;
 end
-result_mat
+result_mat_sc
+
